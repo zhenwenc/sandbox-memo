8ea99f6ce6a426e095aff7825789c73537cff7f2
Frederick Cai <frederick.cai@mattr.global>
2022-08-24 20:27:19 +1200

    fix: breaking changes after upgrade

--- server/config.ts
+++ server/config.ts
@@ -8,8 +8,8 @@ export class AppConfig extends BaseConfig {
   readonly redisURI = this.read('REDIS_URI');
   readonly pusherURI = this.read('PUSHER_URI', null);
 
-  readonly vercelURL = Option.of(this.read('VERCEL_URL', null));
-  readonly publicURL = Option.of(this.read('PUBLIC_URL', null))
+  readonly vercelURL = Option.from(this.read('VERCEL_URL', null));
+  readonly publicURL = Option.from(this.read('PUBLIC_URL', null))
     .orElse(() => this.vercelURL.map(domain => `https://${domain}`))
     .map(resolvePublicURL)
     .getOrElse(Promise.resolve(`http://localhost:${this.port}`));
