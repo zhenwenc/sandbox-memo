58d4da9ad76faec560b001bf37f658b25613d3ee
Frederick Cai <frederick.cai@mattr.global>
2024-05-09 17:54:15 +1200

    fix: webhook events callback ui

--- pages/webhook/events/[uid]/callback.tsx
+++ pages/webhook/events/[uid]/callback.tsx
@@ -5,30 +5,36 @@ import { useRouter } from 'next/router';
 
 export default function CallbackScreen() {
   const router = useRouter();
-  const url = router.asPath;
-  const query = url.includes('?') || !url.includes('#') ? router.query : parse(url.split('#', 2)[1]);
-  const uid = router.query.uid as string;
+  const { asPath: path, basePath } = router;
+  const endpoint = [basePath, '/api/v1', path.split('/callback')[0]].join('');
+  const query = path.includes('?') || !path.includes('#') ? router.query : parse(path.split('#', 2)[1]);
 
   useEffect(() => {
-    console.debug('[CallbackScreen] Parsed params:', { uid, query });
+    console.debug('[CallbackScreen] Parsed params:', { query });
 
     // The page may not be able to read the browser's location when exported as
     // static HTML, or unexpectedly rendered on the server-side.
-    if (!uid || isEmpty(query)) {
-      console.debug('[CallbackScreen] Abort', { uid, query });
+    if (isEmpty(query)) {
+      console.debug('[CallbackScreen] Abort', { query });
       return;
     }
 
-    const params = query as Record<string, string>;
-
-    fetch(`/api/v1/webhook/events/${uid}`, { method: 'POST', body: JSON.stringify(params) })
-      .then(resp => {
-        console.info('[CallbackScreen] Posted query params to server', resp.body);
-      })
-      .catch(error => {
-        console.error('[CallbackScreen] Failed to post data', error);
+    const process = async () => {
+      const resp = await fetch(endpoint, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(query),
       });
-  }, [uid, JSON.stringify(query)]);
+      if (resp.status === 200) {
+        console.info('[CallbackScreen] Posted event to server', await resp.json());
+      } else {
+        console.info('[CallbackScreen] Failed to send event', { status: resp.status });
+      }
+    };
+    process().catch(error => {
+      console.error('[CallbackScreen] Failed to send event', error);
+    });
+  }, [endpoint, JSON.stringify(query)]);
 
-  return <div>'Webhook event received, processing details. You may close this window at any time.'</div>;
+  return <div>{'Webhook event received, processing details. You may close this window at any time.'}</div>;
 }
